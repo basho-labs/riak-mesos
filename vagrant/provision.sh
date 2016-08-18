2915b4d06447d5bc6b7efbbba8e0d6765cfa8a0e
sanmiguel <michael.coles@gmail.com>
2016-08-18 19:27:01 +0200

    Don't set roles for mesos - this isn't needed since at least 0.27

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -20,13 +20,15 @@ update-java-alternatives -s /usr/lib/jvm/java-1.8.0-openjdk-amd64
 apt-get -y install libprotobuf-dev protobuf-compiler
 apt-get -y install marathon
 #apt-get -y install mesos=0.26.0-0.2.145.ubuntu1404
-apt-get -y install mesos=0.26.1-2.0.19.ubuntu1404
+apt-get -y install mesos=0.28.2-2.0.27.ubuntu1404
+# apt-get -y install mesos # 1.0.0
 apt-mark hold mesos
 apt-get -y install lxc-docker
 apt-get -y install resolvconf
 
 # Configure mesos
-echo "riak" > /etc/mesos-master/roles
+# Only need this for mesos < 0.27
+#echo "riak" > /etc/mesos-master/roles
 echo "1" > /etc/mesos-master/quorum
 echo "/var/lib/mesos" > /etc/mesos-master/work_dir
 
